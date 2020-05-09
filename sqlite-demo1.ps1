bbcc4f37d13d9ab4ecb6d348effbec97a349b43b
firstoctet <64554689+firstoctet@users.noreply.github.com>
2020-05-09 17:00:08 -0400

    Many typos are killed with this update

--- sqlite-demo1.ps1
+++ sqlite-demo1.ps1
@@ -1,20 +1,20 @@
-﻿# PowerShell SQLite DB example, muchos gracias to C. Nichols mohawke@gmail.com
+# PowerShell SQLite DB example, muchos gracias to C. Nichols mohawke@gmail.com
 # https://www.darkartistry.com/2019/08/create-insert-and-query-sqlite-with-powershell/
 
 # DOWNLOAD THIS FILE
 #https://system.data.sqlite.org/downloads/1.0.112.0/sqlite-netFx46-binary-x64-2015-1.0.112.0.zip
 
 # Expand-Archive -Path sqlite-netFx46-binary-x64-2015-1.0.112.0.zip -DestinationPath C:\temp
-# copy System.Data.SQLite.dll c:\Suricata
+# copy c:\temp\*dll c:\Suricata
 
 # Unblock-File -Path c:\Suricata\System.Data.SQLite.dll
 
 Add-Type -Path "c:\Suricata\System.Data.SQLite.dll" 
-$DBPath = "c:\Suricata\firstoctet.sqlite"
-Function createDataBase([string]$db){
-    If (!(Test-Path $db)) {
+
+Function createDataBase([string]$DBPath){
+    If (!(Test-Path $DBPath)) {
         $CONN = New-Object -TypeName System.Data.SQLite.SQLiteConnection
-        $CONN.ConnectionString = "Data Source=$db"
+        $CONN.ConnectionString = "Data Source=$DBPath"
         $CONN.Open()
         $CMD = $CONN.CreateCommand()
         $CMD.Dispose()
@@ -22,3 +22,6 @@ Function createDataBase([string]$db){
         Write-Output "Create database and table: Ok"
     } Else {Log-It "DB Exists: Ok"}
 }
+# ******** MAIN ********
+$DBPath = "c:\Suricata\firstoctet.sqlite"
+createDataBase $DBPath  # Function call

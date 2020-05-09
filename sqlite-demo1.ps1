329d798dc13d71f1805dd00776bbdff71b74ef84
firstoctet <64554689+firstoctet@users.noreply.github.com>
2020-05-09 16:41:40 -0400

    Add sqlite-demo1.ps1

---
+++ sqlite-demo1.ps1
@@ -0,0 +1,24 @@
+﻿# PowerShell SQLite DB example, muchos gracias to C. Nichols mohawke@gmail.com
+# https://www.darkartistry.com/2019/08/create-insert-and-query-sqlite-with-powershell/
+
+# DOWNLOAD THIS FILE
+#https://system.data.sqlite.org/downloads/1.0.112.0/sqlite-netFx46-binary-x64-2015-1.0.112.0.zip
+
+# Expand-Archive -Path sqlite-netFx46-binary-x64-2015-1.0.112.0.zip -DestinationPath C:\temp
+# copy System.Data.SQLite.dll c:\Suricata
+
+# Unblock-File -Path c:\Suricata\System.Data.SQLite.dll
+
+Add-Type -Path "c:\Suricata\System.Data.SQLite.dll" 
+$DBPath = "c:\Suricata\firstoctet.sqlite"
+Function createDataBase([string]$db){
+    If (!(Test-Path $db)) {
+        $CONN = New-Object -TypeName System.Data.SQLite.SQLiteConnection
+        $CONN.ConnectionString = "Data Source=$db"
+        $CONN.Open()
+        $CMD = $CONN.CreateCommand()
+        $CMD.Dispose()
+        $CONN.Close()
+        Write-Output "Create database and table: Ok"
+    } Else {Log-It "DB Exists: Ok"}
+}

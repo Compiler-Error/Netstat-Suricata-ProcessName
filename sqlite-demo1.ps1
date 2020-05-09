1d0f808560feaa5a6dcbb9668849ddccf734367d
firstoctet <64554689+firstoctet@users.noreply.github.com>
2020-05-09 18:04:00 -0400

    Adding print lines to show Main, function call, and back to Main

--- sqlite-demo1.ps1
+++ sqlite-demo1.ps1
@@ -4,14 +4,13 @@
 # DOWNLOAD THIS FILE
 #https://system.data.sqlite.org/downloads/1.0.112.0/sqlite-netFx46-binary-x64-2015-1.0.112.0.zip
 
-# unzip the file
+# Unzip it
 # Expand-Archive -Path sqlite-netFx46-binary-x64-2015-1.0.112.0.zip -DestinationPath C:\temp
 # copy c:\temp\*dll c:\Suricata
 
 # Unblock-File -Path c:\Suricata\System.Data.SQLite.dll
 
 Add-Type -Path "c:\Suricata\System.Data.SQLite.dll" 
-
 Function createDataBase([string]$DBPath){
     If (!(Test-Path $DBPath)) {
         $CONN = New-Object -TypeName System.Data.SQLite.SQLiteConnection
@@ -21,8 +20,16 @@ Function createDataBase([string]$DBPath){
         $CMD.Dispose()
         $CONN.Close()
         Write-Output "Create database and table: Ok"
-    } Else {"DB Exists: Ok"}
+    } Else {write-output "DB Exists: Ok"}
 }
 # ******** MAIN ********
+write-output ""
+write-output ""
+write-output "Main"
+write-output "--------------------------"
 $DBPath = "c:\Suricata\firstoctet.sqlite"
+write-output "calling createDatabase"
+write-output "--------------------------"
 createDataBase $DBPath  # Function call
+write-output "--------------------------"
+write-output "Main END"
